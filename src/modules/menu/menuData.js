cbdd70164c31d44252a6e9d9a484c9dec763fad7
BohdanShymko <boghdan.shimko@gmail.com>
2023-07-16 23:39:19 +0300

    feat: gh-pages

--- src/modules/menu/menuData.js
+++
@@ -1,9 +0,0 @@
-import { Menu } from './Menu'
-
-const menuData = new Menu({
-	menu: document.querySelector('#menu'),
-	menuItem: document.querySelector('#menuItem'),
-})
-
-menuData.scroll();
-menuData.click();
--- src/modules/timer/dataTimer.js
+++
@@ -1,9 +0,0 @@
-import { Timer } from './Timer'
-
-const dataTimer = new Timer ({
-	days: document.querySelector('#days'),
-	hours: document.querySelector('#hours'),
-	minutes: document.querySelector('#minutes')
-})
-
-dataTimer.startTimer('2022-10-19');
